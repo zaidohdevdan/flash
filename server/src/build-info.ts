3665e322c9138932f31469c5518ea7e62606330b
Zaidoh <danfealmeidafilho@gmail.com>
2026-02-06 20:58:22 -0300

    chore: add build info to trigger Railway deployment

---
+++ server/src/build-info.ts
@@ -0,0 +1,7 @@
+// Build timestamp: 2026-02-06T20:57:00Z
+// This file forces Railway to rebuild with latest fixes
+export const BUILD_INFO = {
+    timestamp: '2026-02-06T20:57:00Z',
+    commit: 'Latest with type fixes',
+    version: '1.0.1'
+};
